76410de5d4f042a7c113b4db04c335303e457f97
h-banii <h-banii@users.noreply.github.com>
2025-01-18 16:11:41 -0300

    hm-module/plugins: Use listToAttrs and map to import plugins

--- nix/hm-module/plugins/default.nix
+++ nix/hm-module/plugins/default.nix
@@ -1,44 +1,57 @@
 { lib, ... }:
 let
-  inherit (lib) mkOption mkEnableOption;
+  inherit (lib) mkOption mkEnableOption mergeAttrs;
+  inherit (builtins) listToAttrs map;
 in
 {
-  options.programs.youtube-music.plugins = {
-    adblocker = import ./adblocker.nix { inherit lib; };
-    album-actions.enabled = mkEnableOption "Enable Album Actions";
-    album-color-theme = {
-      enabled = mkEnableOption "Enable Album Color Theme";
-      ratio = mkOption { default = 0.5; };
-    };
-    ambient-mode = import ./ambient-mode.nix { inherit lib; };
-    amuse.enabled = mkEnableOption "Enable Amuse";
-    api-server = import ./api-server.nix { inherit lib; };
-    blur-nav-bar = mkEnableOption "Enable Blur Navigation Bar";
-    bypass-age-restrictions = mkEnableOption "Enable Bypass Age Restrictions";
-    captions-selector = {
-      enabled = mkEnableOption "Enable Captions Selector";
-      disableCaptions = mkOption { default = false; };
-      autoload = mkOption { default = false; };
-      lastCaptionsCode = mkOption { default = ""; };
-    };
-    compact-sidebar.enabled = mkEnableOption "Enable Compact Sidebar";
-    crossfade = import ./crossfade.nix { inherit lib; };
-    disable-autoplay = {
-      enabled = mkEnableOption "Enable 'Disable Autoplay'";
-      applyOnce = mkOption { default = false; };
-    };
-    discord = import ./discord.nix { inherit lib; };
-    downloader = import ./downloader.nix { inherit lib; };
-    equalizer = import ./equalizer.nix { inherit lib; };
-    exponential-volume.enabled = mkEnableOption "Enable Exponential Volume plugin";
-    in-app-menu = {
-      enabled = mkEnableOption "Enable In-App Menu plugin";
-      hideDOMWindowControls = mkOption { default = false; };
-    };
-    lumiastream.enabled = mkEnableOption "Enable Lumia Stream plugin";
-    lyrics-genius = {
-      enabled = mkEnableOption "Enable Lyrics Genius plugin";
-      romanizedLyrics = mkOption { default = false; };
-    };
-  };
+  options.programs.youtube-music.plugins =
+    mergeAttrs
+      (listToAttrs (
+        map
+          (plugin: {
+            name = plugin;
+            value = import ./${plugin}.nix { inherit lib; };
+          })
+          [
+            "adblocker"
+            "ambient-mode"
+            "api-server"
+            "crossfade"
+            "discord"
+            "downloader"
+            "equalizer"
+          ]
+      ))
+      {
+        album-actions.enabled = mkEnableOption "Enable Album Actions";
+        album-color-theme = {
+          enabled = mkEnableOption "Enable Album Color Theme";
+          ratio = mkOption { default = 0.5; };
+        };
+        amuse.enabled = mkEnableOption "Enable Amuse";
+        blur-nav-bar = mkEnableOption "Enable Blur Navigation Bar";
+        bypass-age-restrictions = mkEnableOption "Enable Bypass Age Restrictions";
+        captions-selector = {
+          enabled = mkEnableOption "Enable Captions Selector";
+          disableCaptions = mkOption { default = false; };
+          autoload = mkOption { default = false; };
+          lastCaptionsCode = mkOption { default = ""; };
+        };
+        compact-sidebar.enabled = mkEnableOption "Enable Compact Sidebar";
+        disable-autoplay = {
+          enabled = mkEnableOption "Enable 'Disable Autoplay'";
+          applyOnce = mkOption { default = false; };
+        };
+        exponential-volume.enabled = mkEnableOption "Enable Exponential Volume plugin";
+        in-app-menu = {
+          enabled = mkEnableOption "Enable In-App Menu plugin";
+          hideDOMWindowControls = mkOption { default = false; };
+        };
+        lumiastream.enabled = mkEnableOption "Enable Lumia Stream plugin";
+        lyrics-genius = {
+          enabled = mkEnableOption "Enable Lyrics Genius plugin";
+          romanizedLyrics = mkOption { default = false; };
+        };
+      };
+
 }
