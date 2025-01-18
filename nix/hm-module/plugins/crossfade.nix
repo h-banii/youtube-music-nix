0b246d3f5bcae136e2b6bccb29053ed998cf34aa
h-banii <h-banii@users.noreply.github.com>
2025-01-18 00:28:32 -0300

    hm-module: crossfade: Fix root node

--- nix/hm-module/plugins/crossfade.nix
+++ nix/hm-module/plugins/crossfade.nix
@@ -3,20 +3,18 @@ let
   inherit (lib) mkEnableOption mkOption types;
 in
 {
-  crossfade = {
-    enabled = mkEnableOption "Enable Crossfade";
-    fadeInDuration = mkOption { default = 1500; };
-    fadeOutDuration = mkOption { default = 5000; };
-    secondsBeforeEnd = mkOption { default = 10; };
-    fadeScaling = mkOption {
-      default = "linear";
-      type = types.oneOf [
-        (types.enum [
-          "linear"
-          "logarithmic"
-        ])
-        types.int
-      ];
-    };
+  enabled = mkEnableOption "Enable Crossfade";
+  fadeInDuration = mkOption { default = 1500; };
+  fadeOutDuration = mkOption { default = 5000; };
+  secondsBeforeEnd = mkOption { default = 10; };
+  fadeScaling = mkOption {
+    default = "linear";
+    type = types.oneOf [
+      (types.enum [
+        "linear"
+        "logarithmic"
+      ])
+      types.int
+    ];
   };
 }
