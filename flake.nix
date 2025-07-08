f2f5baec269d893fb25febd8f239ddca4e6b607a
h-banii <121690516+h-banii@users.noreply.github.com>
2025-07-08 20:51:31 -0300

    flake: Add lib.mkOptionsDoc

--- flake.nix
+++ flake.nix
@@ -29,42 +29,46 @@
           };
         in
         {
-          homeManagerOptionsDoc =
-            let
-              eval = lib.evalModules {
-                modules = [
-                  {
-                    options._module.args = lib.mkOption {
-                      internal = true;
-                    };
-                    config = {
-                      _module.check = false;
-                    };
-                  }
-                  self.homeManagerModules.default
-                ];
-              };
-            in
-            pkgs.nixosOptionsDoc {
-              # TODO: Fix warnings... (add missing descriptions to ALL options)
-              warningsAreErrors = false;
-              inherit (eval) options;
-            };
+          homeManagerOptionsDoc = self.lib.mkOptionsDoc self.homeManagerModules.default;
           vm = default-system.config.system.build.vm;
         };
 
-      lib.mkYtmSystem =
-        {
-          youtube-music,
-          extraModules ? [ ],
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = {
-            inherit youtube-music;
-            inherit home-manager;
+      lib = {
+        mkOptionsDoc =
+          module:
+          let
+            eval = lib.evalModules {
+              modules = [
+                {
+                  options._module.args = lib.mkOption {
+                    internal = true;
+                  };
+                  config = {
+                    _module.check = false;
+                  };
+                }
+                module
+              ];
+            };
+          in
+          pkgs.nixosOptionsDoc {
+            # TODO: Fix warnings... (add missing descriptions to ALL options)
+            warningsAreErrors = false;
+            inherit (eval) options;
           };
-          modules = [ ./nix/vm/default.nix ] ++ extraModules;
-        };
+        mkYtmSystem =
+          {
+            youtube-music,
+            extraModules ? [ ],
+          }:
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            specialArgs = {
+              inherit youtube-music;
+              inherit home-manager;
+            };
+            modules = [ ./nix/vm/default.nix ] ++ extraModules;
+          };
+      };
     };
 }
